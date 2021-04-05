3e18e827940436ac2b2381b07c12a67f25b7201c
i.perezl.2017 <i.perezl.2017@urjc.es>
2021-04-05 10:29:37 +0200

    Empezando la calculadora

---
+++ P2/index.js
@@ -0,0 +1,44 @@
+console.log("Ejecutando JS...");
+
+
+//-- Elementos de la interfaz de la calculadora
+display = document.getElementById("display")
+boton1 = document.getElementById("boton1")
+boton2 = document.getElementById("boton2")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+
+
+//-- Poner a cero la expresion
+//-- Y volver al estado inicial
+clear.onclick = () => {
+  display.innerHTML = "0";
+  estado = ESTADO.INIT;
+}
+
+// -- Insertar digito 1
+boton1.onclick = () => {
+  display.innerHTML += boton1.value;
+}
+
+//-- Insertar digito 2
+boton2.onclick = () => {
+  display.innerHTML += boton2.value;
+}
+
+//-- Insertar simbolo de sumar
+suma.onclick = () => {
+  display.innerHTML += suma.value;
+}
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = "0";
+}

c2ec58eec9eb3f2f14567aff16cdd7c6af17a431
i.perezl.2017 <i.perezl.2017@urjc.es>
2021-04-06 09:32:31 +0200

    Ejemplo3 canvas

---
+++ P3/L8/canvas-03.js
@@ -0,0 +1,21 @@
+const ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+    //-- Definir un rectangulo de dimensiones 100x50,
+    //-- cuya esquina superior izquierda está en (5,5)
+    ctx.rect(5,5, 100, 50);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Cambiar el tamaño de la linea del trazo
+    ctx.lineWidth = 4;
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+    
+ctx.closePath()

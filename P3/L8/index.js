ebe4640a6353b508f855d679759b9528048f1645
i.perezl.2017 <i.perezl.2017@urjc.es>
2021-04-15 20:32:10 +0200

    avances

--- P3/L8/index.js
+++ P3/L8/index.js
@@ -8,14 +8,108 @@ canvas.height = 900;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
+let xcanvas = 590;
+let ycanvas = 890;
+//Bola
+let xbola = 300;
+let ybola = 850;
+let velxbola = 5;
+let velybola = 3;
+//Raqueta
+let xRaqueta = 300;
+let yRaqueta = 875;
 
-let x = 300;
-let y = 850;
+let contadorpuntos = 0;
 
-let velx = 3;
-let vely = 1;
+function drawbola(){
+      //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+  ctx.arc(xbola, ybola, 10, 0, 2 * Math.PI);
 
-let start = false;
+  //-- Dibujar
+  ctx.fillStyle = 'red';
+
+  //-- Rellenar
+  ctx.fill();
+
+  //-- Dibujar el trazo
+  ctx.stroke()
+ctx.closePath();
+}
+function drawladrillos(){
+
+   //rect1
+  ctx.beginPath();
+   ctx.rect(40,200,50,25);
+   ctx.fillStyle = 'blue';
+   ctx.fill();
+   ctx.stroke()
+ ctx.closePath();
+
+   //rect1
+  ctx.beginPath();
+   ctx.rect(100,200,50,25);
+   ctx.fillStyle = 'red';
+   ctx.fill();
+   ctx.stroke()
+ ctx.closePath();
+
+   //rect1
+  ctx.beginPath();
+   ctx.rect(160,200,50,25);
+   ctx.fillStyle = 'yellow';
+   ctx.fill();
+   ctx.stroke()
+ ctx.closePath();
+
+  //rect 2
+  ctx.beginPath();
+    ctx.rect(220,200,50,25);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+
+  //rect1
+  ctx.beginPath();
+    ctx.rect(280,200,50,25);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+
+  //rect 3
+  ctx.beginPath();
+    ctx.rect(340,200,50,25);
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+
+  //rect 4
+  ctx.beginPath();
+    ctx.rect(400,200,50,25);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+
+  //rect 5
+  ctx.beginPath();
+    ctx.rect(460,200,50,25);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.stroke()
+  ctx.closePath();
+
+   //rect 6
+   ctx.beginPath();
+   ctx.rect(520,200,50,25);
+   ctx.fillStyle = 'red';
+   ctx.fill();
+   ctx.stroke()
+ ctx.closePath();
+}
 
 function update() 
 {
@@ -23,26 +117,35 @@ function update()
   console.log("test");
  
   
-    if (x < 0 || x >= (canvas.width - 20) ) {
-        velx = -velx;
+    if (xbola < 0 || xbola >= xcanvas ) {
+        velxbola = -velxbola;
     }
-    if (y <= 0 || y > canvas.height - 20) {
-        vely = -vely;
+    if (ybola <= 0 || ybola > ycanvas) {
+        velybola = -velybola;
     }
-    if (start == true){
-    x = x + velx;
-    y = y + vely;
-    } 
+
+    xbola = xbola + velxbola;
+    ybola = ybola + velybola;
+
+    if (ybola >= (yRaqueta - 10) && ybola <= (yRaqueta + 100)) {
+    velybola = velybola * -1;
+  }
+
+    if (xRaqueta < 0) {
+      xRaqueta = 0;
+    }
+    if (xRaqueta > 500){
+      xRaqueta = 500;
+    }
 
   //-- 2) Borrar el canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  //-- 3) Dibujar los elementos visibles
   ctx.beginPath();
-    ctx.rect(x, y, 20, 20);
+    ctx.rect(xRaqueta,yRaqueta,100,75);
 
     //-- Dibujar
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'rgb(255, 100, 50)';
 
     //-- Rellenar
     ctx.fill();
@@ -51,19 +154,31 @@ function update()
     ctx.stroke()
   ctx.closePath();
 
+  drawladrillos();
+  drawbola();
   //-- 4) Volver a ejecutar update cuando toque
   requestAnimationFrame(update);
 }
 
-document.onkeydown = (e) => {
-    console.log("were")
-    switch (e.key) {
-      case 13 :
-          start == true;
-          console.log("algo");
-      break;
-      }
-  }
 
-//-- ¡Que empiece la función!
+window.onkeydown = (e) => {
+  //-- Según la tecla se hace una cosa u otra
+  switch (e.key) {
+    case "a":
+      xRaqueta = xRaqueta - 10;
+    break;
+
+    case "d":
+      xRaqueta = xRaqueta + 10;
+    break;
+  }
+}
+
+
+
+window.onkeyup = (e) => {
+  if (e.key == "a" || e.key == "d") {
+      xRaqueta = xRaqueta;
+  }
+}
 update();

b11b49310d90ccff48461b5ff8c006b3efd94340
i.perezl.2017 <i.perezl.2017@urjc.es>
2021-05-30 17:10:02 +0200

    funcinamiento general

---
+++ P5/index.js
@@ -0,0 +1,96 @@
+let directo = document.getElementById("directo");
+const video1 = document.getElementById("video1");
+const btn_video1 = document.getElementById("btn_video1");
+const video2 = document.getElementById("video2");
+const btn_video2 = document.getElementById("btn_video2");
+const video3 = document.getElementById("video3");
+const btn_video3 = document.getElementById("btn_video3");
+const btn_test = document.getElementById("btn_test");
+const btn_src_on = document.getElementById("btn_src_on");
+const btn_src_off = document.getElementById("btn_src_off");
+
+
+//-- Establecer las dimensiones de los vídeos
+directo.width=620;
+directo.height=400;
+video1.width=400;  
+video1.height=200;
+video2.width=400;  
+video2.height=200;
+video3.width=400;  
+video3.height=200;
+
+//-- Imagen de Test usada
+const TEST_IMAGE_URL = "standby.jpg";
+
+//-- Imagen estática a mostrar cuando el video no
+//-- ha arrancado
+directo.poster = TEST_IMAGE_URL;
+video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
+
+//-- Boton de FUENTES-ON
+btn_src_on.onclick = () => {
+ 
+  //-- Establecer la fuente de la cámara 1
+  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+  video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
+
+  //-- Reprodeucimos un vídeo, desde el comienzo
+  video1.currentTime = 0;
+  video1.play();
+  video1.muted;
+
+  video2.currentTime = 0;
+  video2.play();
+  video2.muted;
+
+  video3.currentTime = 0;
+  video3.play();
+  video3.muted;
+
+  //-- En la emisión en directo ponemos la imagen de prueba
+  directo.poster = TEST_IMAGE_URL;
+};
+
+btn_src_off.onclick = () => {
+ 
+  
+    //-- Reprodeucimos un vídeo, desde el comienzo
+    video1.currentTime = 0;
+    video1.play();
+  
+    //-- Y en silencio...
+    video1.muted;
+  
+    //-- En la emisión en directo ponemos la imagen de prueba
+    directo.poster = TEST_IMAGE_URL;
+  };
+
+//-- Botón de Test
+btn_test.onclick = () => {
+    directo.poster = TEST_IMAGE_URL;
+    directo.src = null;
+};
+
+//-- Botón de Selección de la cámara 1
+btn_video1.onclick = () => {
+    directo.src = video1.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+btn_video3.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
